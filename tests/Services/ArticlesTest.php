fa804a1a98446a888dc7cb004fc9056fdc21ff61
Olivier Bernard <olivier.bernard.1492@gmail.com>
2022-05-06 03:52:59 +0200

    WIP: adding test for TDD

---
+++ tests/Services/ArticlesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Services;
+
+use PHPUnit\Framework\TestCase;
+
+class ArticlesTest extends TestCase
+{
+    public function setUp(): void {
+    }
+
+    public function testAssert() {
+        $this->assertTrue(true);
+    }
+}
